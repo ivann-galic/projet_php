8567561ba96702a57c890baa3b1ccfaf745d7b44
cassel boris <casselboris92@gmail.com>
2020-01-22 13:32:06 +0100

    index oublie

--- index.php
+++ index.php
@@ -1,7 +1,15 @@
 <?php
+
+    include "header.php";
     require "Backlog.php";
 
-    $members = new Backlog();
+
+echo '<div class="espaceCarte">
+		<div class="titreStory"><p>Story 1</p></div>
+		<div class="carte"></div>
+	</div>'
+
+$members = new Backlog();
     echo $members -> displayMembers("Antoine", "Boris", "Ivann");
     echo $members -> displayStories(1, 16);
 
@@ -14,5 +22,13 @@
     array_push($listPendingStories, "story 2", "story 3", "story 4", "story 5", "story 6", "story 7", "story 8",
         "story 9", "story 10", "story 11", "story 12", "story 13", "story 14", "story 15", "story 16");
     $stories -> displayPendingStories($listPendingStories);
+
+
+
+    echo '</body>
+<script src="https://code.jquery.com/jquery-3.4.1.js" integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU=" crossorigin="anonymous"></script>
+	<script src="js/app.js"></script>
+</html>'
+
 
 ?>
