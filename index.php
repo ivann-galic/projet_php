c846a1ff4214f4e7f7f4d9074459a0f871c796bc
cassel boris <casselboris92@gmail.com>
2020-01-22 13:49:42 +0100

    fin story 1

--- index.php
+++ index.php
@@ -5,9 +5,8 @@
 
 
 echo '<div class="espaceCarte">
-		<div class="titreStory"><p>Story 1</p></div>
-		<div class="carte"></div>
-	</div>'
+            <div class="titreStory"><p>Story 1</p></div>
+            <div class="carte">';
 
 $members = new Backlog();
     echo $members -> displayMembers("Antoine", "Boris", "Ivann");
@@ -22,6 +21,10 @@ $members = new Backlog();
     array_push($listPendingStories, "story 2", "story 3", "story 4", "story 5", "story 6", "story 7", "story 8",
         "story 9", "story 10", "story 11", "story 12", "story 13", "story 14", "story 15", "story 16");
     $stories -> displayPendingStories($listPendingStories);
+
+    echo '</div>
+	</div>';
+
 
 
 
