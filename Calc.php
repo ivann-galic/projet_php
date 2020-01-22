4856014d0905ff07deaaa7eb5d3a78b53e0fa8cf
Ivann <ivann.galic@edu.itescia.fr>
2020-01-22 21:32:25 +0100

    story ok (correction)

--- Calc.php
+++ Calc.php
@@ -4,15 +4,16 @@ Class Calc
 
     function affichePremiers($n){
         echo "Les nombres premiers entre 0 et ".$n." sont : ";
-        $negatif = false;
+        $negative = false;
         if($n<0){
-            $negatif = true;
+            $negative = true;
             $n = -$n;
         }
-        //On prend chaque nombre entre 2 et n (0 et 1 n'étant pas premier)
+
         for($i=2;$i<=$n;$i++){
-            $isPrimeNumber = false;
-            $nbDiv = 0;//Et on compte le nombre de diviseur
+
+            $nbDiv = 0;
+            // Counts the number of divisors
             for($j=1;$j<=$i;$j++){
                 if($i%$j==0){
                     $nbDiv++;
@@ -20,10 +21,7 @@ Class Calc
                 }
             }
             if($nbDiv == 2){
-                $isPrimeNumber = false;
-                //Un nombre premier est un chiffre qui ne possède que 2 diviseur (1 et
-                // lui-même)
-                if($negatif){
+                if($negative){
                     echo "-";
                 }
                 echo $i.", ";
@@ -34,31 +32,5 @@ Class Calc
         }
     }
 
-    /*function displayNbPremiers($n)
-    {
-        echo "Les nombres premiers entre 0 et " . $n . " sont : ";
-
-        $negatif = false;
-        if ($n < 0) {
-            $negatif = true;
-            $n = -$n;
-        }
-        //On prend chaque nombre entre 2 et n (0 et 1 n'étant pas premier)
-        for ($i = 2; $i <= $n; $i++) {
-            $nbDiv = 0;//Et on compte le nombre de diviseur
-            for ($j = 1; $j <= $i; $j++) {
-                if ($i % $j == 0) {
-                    $nbDiv++;
-                }
-            }
-            if ($nbDiv == 2) {
-                //Un nombre premier est un chiffre qui ne possède que 2 diviseur (1 et lui-même)
-                if ($negatif) {
-                    echo "-";
-                }
-                echo $i . ", ";
-            }
-        }
-    }*/
 }
 ?>
